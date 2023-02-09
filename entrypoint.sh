b51c950d40ec09f3c1fdcebe9ec959d88d6df724
SDKHISSI <admin.sdkhissi@slanlp0033.ad.ponet>
2023-02-09 14:45:45 +0100

    fin d'etape

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,13 @@
 
 composer require asset symfony/apache-pack debug templates symfony/ux-turbo
 composer require --dev symfony/profiler-pack 
-
 yarn add bootstrap jquery @popperjs/core @fontsource/roboto-condensed @fortawesome/fontawesome-free axios --dev
-
 composer install 
 yarn install
-
 composer clear-cache
-
 chmod 777 . 
+
 yarn watch &
-exec apache2-foreground &
+# yarn encore production &
+
+exec apache2-foreground 
