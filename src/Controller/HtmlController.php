4dca87a922699217a34b716d2b420cb49e6e465e
SDKHISSI <admin.sdkhissi@slanlp0033.ad.ponet>
2023-02-13 14:15:03 +0100

    truc

--- src/Controller/HtmlController.php
+++ src/Controller/HtmlController.php
@@ -7,89 +7,58 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use function Symfony\Component\String\u;
 
 
 #[Route('/', name: 'app_')]
 
 
+
 class HtmlController extends AbstractController
 {
-    #[Route('/', name: 'base')]
+private $zones = [
+    ['ligne' => 'D41', 'zones' => 'Assemblage'],
+    ['ligne' => 'Démontage', 'zones' => 'Assemblage'],
+    ['ligne' => 'Enjoliveur', 'zones' => 'Assemblage'],
+    ['ligne' => 'D41', 'zones' => 'Déchargement Reprise'],
+    ['ligne' => 'DEFAUTHEQUE', 'zones' => 'Déchargement Reprise'],
+];
+// private $zone = [array_column($zones, 'zones')];
 
+    #[Route('/', name: 'base')]
     public function base(): Response
     {
         return $this->render('base.html.twig');
     }
 
-
     #[Route('/index', name: 'index')]
-
     public function index(): Response
     {
-        // return $this->render('html/index.html.twig');
-        $zones = [
-            ['ligne' => 'D41', 'zones' => 'Assemblage'],
-            ['ligne' => 'Démontage', 'zones' => 'Assemblage'],
-            ['ligne' => 'Enjoliveur', 'zones' => 'Assemblage'],
-            ['ligne' => 'D41', 'zones' => 'Déchargement Reprise'],
-            ['ligne' => 'DEFAUTHEQUE', 'zones' => 'Déchargement Reprise'],
-        
-            // 'Demontage - Assemblage',
-            // 'Enjoliveur P51 - Assemblage',
-            // 'P54 - Assemblage',
-            // 'P8 MV - Assemblage',
-            // 'Panneaux - Assemblage',
-            // 'T9 MV - Assemblage',
-            // 'D41 - Déchargement Reprise',
-            // 'DEFAUTHEQUE - Déchargement Reprise',
-            // 'GESTION-DR - Déchargement Reprise',
-            // 'LUX-DR - Déchargement Reprise',
-            // 'MATRICES-ILUO - Déchargement Reprise',
-            // 'P87 - Déchargement Reprise',
-        ];
         return $this->render('html/index.html.twig', [
-        'title' => 'DocAuPoste',
-        'zones' => $zones]);
-    }
-
-    #[Route('/sommere', name: 'sommere')]
-
-    public function sommere(): Response
-    
-    {
-        // return $this->render('html/sommere.html.twig');
-        $zones = [
-            ['ligne' => 'D41', 'zones' => 'Assemblage'],
-            ['ligne' => 'Démontage', 'zones' => 'Assemblage'],
-            ['ligne' => 'Enjoliveur', 'zones' => 'Assemblage'],
-            ['ligne' => 'D41', 'zones' => 'Déchargement Reprise'],
-            ['ligne' => 'DEFAUTHEQUE', 'zones' => 'Déchargement Reprise'],
-        
-            // 'Demontage - Assemblage',
-            // 'Enjoliveur P51 - Assemblage',
-            // 'P54 - Assemblage',
-            // 'P8 MV - Assemblage',
-            // 'Panneaux - Assemblage',
-            // 'T9 MV - Assemblage',
-            // 'D41 - Déchargement Reprise',
-            // 'DEFAUTHEQUE - Déchargement Reprise',
-            // 'GESTION-DR - Déchargement Reprise',
-            // 'LUX-DR - Déchargement Reprise',
-            // 'MATRICES-ILUO - Déchargement Reprise',
-            // 'P87 - Déchargement Reprise',
-        ];
-        // Method to dump the specified variables : 
-        // dump($zones);
-        // dd($zones);
-
-        return $this->render('html/sommere.html.twig', [
             'title' => 'DocAuPoste',
-            'zones' => $zones,
-        // $html = $twig->render('Dap/homepage.html.twig', [
-        //     'title' => 'DocAuPoste',
-        //     'zones' => $zones,
+            'zones' => $this->zones,
         ]);
     }
 
+    #[Route('/sommere', name: 'sommere')]
+    public function sommere(): Response
+    {
+        return $this->render('html/sommere.html.twig', [
+            'title' => 'DocAuPoste',
+            'zones' => $this->zones,
+        ]);
+    }
+
+    // #[Route('/sommere/{slug}', name: 'browse')]
+    // public function browse(string $slug = null): Response
+    // {
+    //     $zoneList = array_column($this->zones, 'zones');
+
+    //     $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+    //     return $this->render('html/sommere.html.twig', [
+    //         // 'title' => $title,
+    //         'zone' => $zoneList,
+    //     ]);
+    // }
 }
