9f8cda9a958e53f1000afe83c32167e3143f02d6
SDKHISSI <admin.sdkhissi@slanlp0033.ad.ponet>
2023-02-17 14:06:45 +0100

    plus de truc

--- templates/php/demo.php
+++ templates/php/demo.php
@@ -1,8 +1,10 @@
-<!-- {% extends '/php/basephp.html.twig' %}
+ /*
+ <!-- {% extends '/php/basephp.html.twig' %}
 {% block body %}
 {% endblock %} -->
-
-<?php
+ */
+ <?php
+    /*
 $prenom = 'Jean';
 $nom = 'Dupont';
 $note = 12;
@@ -108,13 +110,220 @@ if ($note3 >= 10) {
 }
 
 echo "\n";
+
 echo "\n Ajout d'une readline pour effectuer une entrée de donnée \n";
+
 $note4 = readline("Entrez une note : ");
+
 echo "\n";
+
 echo "\n";
+
 if ($note4 >= 10) {
     echo "Vous avez la moyenne";
 } else {
     echo "Vous n'avez pas la moyenne";
 }
-?>
+echo "\n";
+
+echo "\nAjout d'une sous condition\n";
+
+echo "\n";
+
+$note5 = readline("Entrez une note : ");
+if ($note5 >= 10) {
+    if ($note5 == 10) {
+        echo "Vous avez juste la moyenne";
+    } else {
+        echo "Vous avez plus que la moyenne";
+    }
+} else {
+    echo "Vous n'avez pas la moyenne";
+}
+
+echo "\n";
+echo "\nAjout d'une condition avec elseif\n";
+echo "\n";
+
+if ($note5 > 10) {
+    echo "Bravo vous avez plus que la moyenne";
+} elseif ($note5 == 10) {
+    echo "Vous avez juste la moyenne";
+} else {
+    echo "Vous n'avez pas la moyenne";
+}
+echo "\n";
+echo "\n Ajout du triple egal pour verifier la valeur et le type \n";
+echo "\n";
+
+if ($note5 > 10) {
+    echo "Bravo vous avez plus que la moyenne";
+} elseif ($note5 === 10) {
+    echo "Vous avez juste la moyenne";
+} else {
+    echo "Vous n'avez pas la moyenne";
+}
+
+echo "\n";
+echo "\nAjout de la conversion du type en indiquant le type entre parenthese avant la valeur pour eviter une erreur avec le triple egal. \n";
+echo "\n";
+
+$note6 = (int)readline("Entrez une note : ");
+echo "\n";
+if ($note6 > 10) {
+    echo "Bravo vous avez plus que la moyenne";
+} elseif ($note6 === 10) {
+    echo "Vous avez juste la moyenne";
+} else {
+    echo "Vous n'avez pas la moyenne";
+}
+
+echo "\n";
+echo "\n";
+echo "\n";
+echo "\nTest d'une collection de valeur. En specifiant bien le type de valeur dans la variable. Avec une suite de test elseif. \n";
+echo "\n";
+
+$action = (int)readline("Entrez une action (1: attaquer, 2: defendre, 3: passer mon tour : ");
+if ($action === 1) {
+    echo "Vous attaquez";
+} elseif ($action === 2) {
+    echo "Vous defendez";
+} elseif ($action === 3) {
+    echo "Vous passez votre tour";
+} else {
+    echo "Vous n'avez pas choisi une action valide";
+}
+
+echo "\n";
+echo "\n";
+echo "\n";
+echo "\nTest d'une collection de valeur. En specifiant bien le type de valeur dans la variable. Avec un switch. \n";
+echo "\n";
+
+$action1 = (int)readline("Entrez une action (1: attaquer, 2: defendre, 3: passer mon tour) : ");
+
+switch ($action1) {
+    case 1:
+        echo "Vous attaquez";
+        break;
+    case 2:
+        echo "Vous defendez";
+        break;
+    case 3:
+        echo "Vous passez votre tour";
+        break;
+    default:
+        echo "Vous n'avez pas choisi une action valide";
+        break;
+}
+
+
+
+    echo "\n";
+    echo "\n";
+    echo "\n";
+    echo "\nTest pour voir si un magasin est ouvert en testant l'heure donner aux horaires d'ouverture. \n";
+    echo "\n";
+
+
+    $heure = (int)readline("Entrez une heure : ");
+
+    if ($heure >= 9 && $heure <= 12 || $heure >= 14 && $heure <= 18) {
+        echo "Le magasin est ouvert";
+    } else {
+        echo "Le magasin est fermé";
+    }
+   
+    /*
+VRAI && VRAI = VRAI
+VRAI && FAUX = FAUX
+FAUX && VRAI = FAUX
+FAUX && FAUX = FAUX
+
+VRAI || VRAI = VRAI
+VRAI || FAUX = VRAI
+FAUX || VRAI = VRAI
+FAUX || FAUX = FAUX
+*/
+    /*
+    echo "\n";
+    echo "\n";
+    echo "\n";
+
+    echo "\n";
+    echo "\n";
+    echo "\nTest pour voir si un magasin est ouvert en testant l'heure donner aux horaires d'ouverture. En inversant les choses et en adaptant les conditions.  \n";
+    echo "\n";
+
+
+    $heure = (int)readline("Entrez une heure : ");
+
+    if ($heure < 9 || $heure > 12 && $heure < 14 || $heure > 18) {
+        echo "Le magasin est fermé";
+    } else {
+        echo "Le magasin est ouvert";
+    }
+
+    echo "\n";
+    echo "\n";
+
+    if (!($heure >= 9 && $heure <= 12 || $heure >= 14 && $heure <= 18)) {
+        echo "Le magasin est fermé";
+    } else {
+        echo "Le magasin est ouvert";
+    }
+    echo "\n";
+    echo "\n";
+ 
+    echo "\n";
+    echo "\n";
+    echo "\nDebut sur les loop avec While.  \n";
+    echo "\n";
+
+    $chiffre = null;
+
+    while ($chiffre !== 42) {
+        $chiffre = (int)readline("Entrez un chiffre : ");
+    }
+    echo "Bravo vous avez trouvé le chiffre mystère";
+  
+    echo "\n";
+    echo "\n";
+    echo "\nLoop avec for.  \n";
+    echo "\n";
+
+
+
+    for ($i = 0; $i < 10; $i++) {
+        echo "- $i \n";
+    }
+    echo "\n";
+    echo "\nEn utilisant le $i = $i + 2.  \n";
+    for ($i = 0; $i < 10; $i = $i + 2) {
+        echo "- $i \n";
+    }
+    echo "\n";
+    echo "\nEn utilisant le += 2.  \n";
+    for ($i = 0; $i < 10; $i += 2) {
+        echo "- $i \n";
+    }
+    echo "\n";
+    echo "\nEn utilisant le -= 2 avec une seconde expression inverser à -10 plutot que 10  \n";
+    for ($i = 0; $i > -10; $i -= 2) {
+        echo "- $i \n";
+    }
+ */
+    echo "\n";
+    echo "\n";
+    echo "\nLoop avec for pour afficher une liste complete.   \n";
+    echo "\n";
+
+    $notes = [10, 12, 8, 20, 18, 16, 14];
+
+    for ($i = 0; $i < count($notes); $i++) {
+        echo "- $notes[$i]" . "\n";
+    }
+    echo "\n";
+
+    ?>
