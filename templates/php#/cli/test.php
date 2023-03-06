9552b30e19177e560b0aabc648caa4867122fb42
SDKHISSI <admin.sdkhissi@slanlp0033.ad.ponet>
2023-03-06 17:12:01 +0100

    pushe

---
+++ templates/php#/cli/test.php
@@ -0,0 +1,8 @@
+<?php
+// var_dump(__DIR__);
+// var_dump(dirname(__DIR__));
+// $fichier = __DIR__ . DIRECTORY_SEPARATOR . 'demo.txt';
+// // file_put_contents($fichier, 'Hello World');
+// file_put_contents($fichier, 'Hello World', FILE_APPEND);
+
+// file_get_contents($fichier); 
