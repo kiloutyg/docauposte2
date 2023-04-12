f3fcb4fa11756e451c30c825fc60e4f6424c1797
SFDKHISSI <floriandkhissi@gmail.com>
2023-04-12 10:02:15 +0200

    new branching

--- src/Controller/MasterController.php
+++ src/Controller/MasterController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\AdminController;
+use App\Controller\SecurityController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class MasterController extends AbstractController
 {
     #[Route('/master', name: 'app_master')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('master/index.html.twig', [
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+
+        return $this->render('master/master_index.html.twig', [
             'controller_name' => 'MasterController',
+            'error' => $error,
+            'last_username' => $lastUsername,
+
+
+
         ]);
     }
 }
