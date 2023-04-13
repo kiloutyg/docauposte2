7c8d209593b05b7523f6b68d92cb19ed184aad1a
SFDKHISSI <floriandkhissi@gmail.com>
2023-04-13 09:22:08 +0200

    added a cheat code to create the super user

--- migrations/Version20230412155203.php
+++ migrations/Version20230412155203.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230411113214 extends AbstractMigration
+final class Version20230412155203 extends AbstractMigration
 {
     public function getDescription(): string
     {
