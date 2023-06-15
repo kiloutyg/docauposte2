a4cc9fc91a8fc6e0e5caa55d976c2f8c25692bbc
kiloutyg <sfdkhissi@protonmail.com>
2023-06-15 21:29:44 +0200

    stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p ./public/doc 
-export http_proxy='http://10.0.0.1:80'
+# export http_proxy='http://10.0.0.1:80'
 composer require --dev symfony/asset symfony/apache-pack symfony/error-handler symfony/templating symfony/ux-turbo symfony/profiler-pack symfony/var-dumper symfony/webpack-encore-bundle 
 yarn add bootstrap jquery @popperjs/core sass-loader  sass @fontsource/roboto-condensed @fortawesome/fontawesome-free axios core-js webpack encore webpack-cli webpack-notifier @symfony/webpack-encore favicon --dev
 composer install
