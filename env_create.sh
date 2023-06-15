f191ff01981aeb17613a64936185669d08005d0a
kiloutyg <sfdkhissi@protonmail.com>
2023-06-16 00:11:31 +0200

    finished script for changing password and secret

--- env_create.sh
+++ env_create.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-
-
-
 # Prompt for database details
-read -p "Please enter your MySQL username: " db_username
-read -p "Please enter your MySQL password: " db_password
-read -p "Please enter your database name: " db_name
-# read -p "Please enter your server version (e.g. mariadb-10.4.13): " MariaDB- . server_version
-
-# Generate a random secret
-app_secret=$(openssl rand -hex 16)
-# DATABASE_URL=mysql://\${MYSQL_USER}:\${MYSQL_PASSWORD}@\${DB_HOST}/\${MYSQL_DATABASE}?serverVersion=${server_version}
+read -p "Please enter your MySQL root password: " MYSQL_ROOT_PASSWORD
+read -p "Please enter your MySQL username: " MYSQL_USER
+read -p "Please enter your MySQL password: " MYSQL_PASSWORD
+read -p "Please enter your database name: " MYSQL_DATABASE
+# APP_SECRET=96ae0f3daef954cfbcb61ad63652ca85
+APP_SECRET=$(openssl rand -hex 16)
 
 # Create .env file
 cat > .env <<EOL
-# .env
-
-
-# MySQL settings
-MYSQL_ROOT_PASSWORD=$(openssl rand -hex 8)
-MYSQL_DATABASE=${db_name}
-MYSQL_USER=${db_username}
-MYSQL_PASSWORD=${db_password}
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+MYSQL_DATABASE=${MYSQL_DATABASE}
+MYSQL_USER=${MYSQL_USER}
+MYSQL_PASSWORD=${MYSQL_PASSWORD}
 
 ###> symfony/framework-bundle ###
 APP_ENV=prod
-APP_SECRET=${app_secret}
+APP_SECRET=${APP_SECRET}
 SYMFONY_DEPRECATIONS_HELPER=weak
 ###< symfony/framework-bundle ###
 
@@ -40,7 +31,9 @@ MESSENGER_TRANSPORT_DSN=doctrine://default?auto_setup=0
 #
 # DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db"
 # DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=8&charset=utf8mb4"
-DATABASE_URL=mysql://root:${MYSQL_ROOT_PASSWORD}@database/${MYSQL_DATABASE}?serverVersion=MariaDB-10.11.4
+
+DATABASE_URL=mysql://root:\${MYSQL_ROOT_PASSWORD}@database/\${MYSQL_DATABASE}?serverVersion=MariaDB-10.11.4
+
 ###< doctrine/doctrine-bundle ###
 
 ###> symfony/messenger ###
@@ -55,6 +48,44 @@ DATABASE_URL=mysql://root:${MYSQL_ROOT_PASSWORD}@database/${MYSQL_DATABASE}?serv
 ###< symfony/mailer ###
 EOL
 
-echo ".env.prod file created successfully!"
+echo ".env file created successfully!"
 
+cat > .env.local <<EOL
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+MYSQL_DATABASE=${MYSQL_DATABASE}
+MYSQL_USER=${MYSQL_USER}
+MYSQL_PASSWORD=${MYSQL_PASSWORD}
 
+###> symfony/framework-bundle ###
+APP_ENV=prod
+APP_SECRET=${APP_SECRET}
+###< symfony/framework-bundle ###
+
+###> symfony/webapp-pack ###
+MESSENGER_TRANSPORT_DSN=doctrine://default?auto_setup=0
+###< symfony/webapp-pack ###
+
+###> doctrine/doctrine-bundle ###
+# Format described at https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connecting-using-a-url
+# IMPORTANT: You MUST configure your server version, either here or in config/packages/doctrine.yaml
+#
+# DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db"
+# DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=8&charset=utf8mb4"
+
+DATABASE_URL=mysql://root:\${MYSQL_ROOT_PASSWORD}@database/\${MYSQL_DATABASE}?serverVersion=MariaDB-10.11.4
+
+###< doctrine/doctrine-bundle ###
+
+###> symfony/messenger ###
+# Choose one of the transports below
+# MESSENGER_TRANSPORT_DSN=doctrine://default
+# MESSENGER_TRANSPORT_DSN=amqp://guest:guest@localhost:5672/%2f/messages
+# MESSENGER_TRANSPORT_DSN=redis://localhost:6379/messages
+###< symfony/messenger ###
+
+###> symfony/mailer ###
+# MAILER_DSN=null://null
+###< symfony/mailer ###
+EOL
+
+echo ".env file created successfully!"
--- migrations/Version20230615204224.php
+++ migrations/Version20230615204224.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230613192845 extends AbstractMigration
+final class Version20230615204224 extends AbstractMigration
 {
     public function getDescription(): string
     {
