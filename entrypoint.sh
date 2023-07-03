4f18eb51d2162c2f36b88203086d04c99e831555
kiloutyg <sfdkhissi@protonmail.com>
2023-07-03 22:35:59 +0200

    deployment strategy improvement and starter to update front

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,102 +1,23 @@
 #!/bin/sh
-mkdir -p ./public/doc 
-export http_proxy='http://10.0.0.1:80'
-composer require --dev    symfony/maker-bundle \
-                        symfony/orm-pack \
-                        symfony/asset \
-                        symfony/apache-pack \
-                        symfony/error-handler \
-                        symfony/templating \
-                        symfony/ux-turbo \
-                        symfony/profiler-pack \
-                        symfony/var-dumper \
-                        symfony/webpack-encore-bundle \
-                        doctrine/annotations \
-                        doctrine/doctrine-bundle \
-                        doctrine/doctrine-migrations-bundle \
-                        doctrine/orm \
-                        phpdocumentor/reflection-docblock \
-                        phpstan/phpdoc-parser \
-                        symfony/apache-pack \
-                        symfony/asset \
-                        symfony/console \
-                        symfony/doctrine-messenger \
-                        symfony/dotenv \
-                        symfony/error-handler \
-                        symfony/expression-language \
-                        symfony/flex \
-                        symfony/form \
-                        symfony/framework-bundle \
-                        symfony/http-client \
-                        symfony/http-foundation \
-                        symfony/intl \
-                        symfony/mailer \
-                        symfony/mime \
-                        symfony/monolog-bundle \
-                        symfony/notifier \
-                        symfony/process \
-                        symfony/property-access \
-                        symfony/property-info \
-                        symfony/runtime \
-                        symfony/security-bundle \
-                        symfony/serializer \
-                        symfony/string \
-                        symfony/templating \
-                        symfony/translation \
-                        symfony/twig-bundle \
-                        symfony/ux-turbo \
-                        symfony/validator \
-                        symfony/var-dumper \
-                        symfony/web-link \
-                        symfony/webpack-encore-bundle \
-                        symfony/yaml \
-                        twig/extra-bundle \
-                        twig/twig \
-                        doctrine/doctrine-fixtures-bundle \
-                        phpunit/phpunit \
-                        symfony/browser-kit \
-                        symfony/css-selector \
-                        symfony/debug-bundle \
-                        symfony/maker-bundle \
-                        symfony/phpunit-bridge \
-                        symfony/stopwatch \
-                        symfony/web-profiler-bundle
-        
 
-yarn add --dev bootstrap \
-                jquery \
-                @popperjs/core \
-                sass-loader \
-                sass \
-                @fontsource/roboto-condensed \
-                @fortawesome/fontawesome-free \
-                axios \
-                core-js \
-                webpack \
-                encore \
-                webpack-cli \
-                webpack-notifier \
-                @symfony/webpack-encore \
-                @symfony/ux-turbo \
-                @symfony/stimulus-bridge \
-                @symfony/stimulus-bundle \
-                regenerator-runtime \
-                favicon \
-                @babel/core \
-                @babel/preset-env \
-                @hotwired/turbo \
-                @hotwired/stimulus \
-                node-sass
-                
+# export http_proxy='http://10.0.0.1:80';
+composer require --dev symfony/asset symfony/apache-pack symfony/error-handler symfony/templating symfony/ux-turbo symfony/profiler-pack symfony/var-dumper symfony/webpack-encore-bundle  symfony/ux-turbo;
+# composer require symfony/asset symfony/apache-pack symfony/error-handler symfony/templating symfony/ux-turbo symfony/profiler-pack symfony/var-dumper symfony/webpack-encore-bundle  symfony/ux-turbo;
+yarn add bootstrap jquery @popperjs/core sass-loader  sass @fontsource/roboto-condensed @fortawesome/fontawesome-free axios core-js webpack encore webpack-cli webpack-notifier @symfony/webpack-encore favicon --dev;
+# yarn add bootstrap jquery @popperjs/core sass-loader  sass @fontsource/roboto-condensed @fortawesome/fontawesome-free axios core-js webpack encore webpack-cli webpack-notifier @symfony/webpack-encore favicon;
+# composer install --no-dev --optimize-autoloader;
 
-composer install &&
-composer update  &&
-yarn install
-yarn upgrade
-composer clear-cache
-chmod 777 . -R -v
-php bin/console make:migration
-php bin/console doctrine:migrations:migrate 
+composer install;
+composer update ;
+yarn install;
+yarn upgrade;
+composer clear-cache;
+chmod 777 . -R -v;
+# yarn encore production &
+php bin/console make:migration;
+php bin/console doctrine:migrations:migrate;
 
 exec apache2-foreground  &
+# yarn watch &
 yarn encore dev --watch
+# yarn run encore production --progress
--- env_create.sh
+++ env_create.sh
@@ -5,6 +5,7 @@ read -p "Please enter your MySQL root password: " MYSQL_ROOT_PASSWORD
 read -p "Please enter your MySQL username: " MYSQL_USER
 read -p "Please enter your MySQL password: " MYSQL_PASSWORD
 read -p "Please enter your database name: " MYSQL_DATABASE
+read -p "Please enter the app context (prod/dev): " APP_CONTEXT
 # APP_SECRET=96ae0f3daef954cfbcb61ad63652ca85
 APP_SECRET=$(openssl rand -hex 16)
 
@@ -16,7 +17,7 @@ MYSQL_USER=${MYSQL_USER}
 MYSQL_PASSWORD=${MYSQL_PASSWORD}
 
 ###> symfony/framework-bundle ###
-APP_ENV=prod
+APP_ENV=${APP_CONTEXT}
 APP_SECRET=${APP_SECRET}
 ###< symfony/framework-bundle ###
 
@@ -47,44 +48,5 @@ DATABASE_URL=mysql://root:\${MYSQL_ROOT_PASSWORD}@database/\${MYSQL_DATABASE}?se
 ###< symfony/mailer ###
 EOL
 
-echo ".env file created successfully!"
-
-cat > .env.local <<EOL
-MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
-MYSQL_DATABASE=${MYSQL_DATABASE}
-MYSQL_USER=${MYSQL_USER}
-MYSQL_PASSWORD=${MYSQL_PASSWORD}
-
-###> symfony/framework-bundle ###
-APP_ENV=prod
-APP_SECRET=${APP_SECRET}
-###< symfony/framework-bundle ###
-
-###> symfony/webapp-pack ###
-MESSENGER_TRANSPORT_DSN=doctrine://default?auto_setup=0
-###< symfony/webapp-pack ###
-
-###> doctrine/doctrine-bundle ###
-# Format described at https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connecting-using-a-url
-# IMPORTANT: You MUST configure your server version, either here or in config/packages/doctrine.yaml
-#
-# DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db"
-# DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=8&charset=utf8mb4"
-
-DATABASE_URL=mysql://root:\${MYSQL_ROOT_PASSWORD}@database/\${MYSQL_DATABASE}?serverVersion=MariaDB-10.11.4
-
-###< doctrine/doctrine-bundle ###
-
-###> symfony/messenger ###
-# Choose one of the transports below
-# MESSENGER_TRANSPORT_DSN=doctrine://default
-# MESSENGER_TRANSPORT_DSN=amqp://guest:guest@localhost:5672/%2f/messages
-# MESSENGER_TRANSPORT_DSN=redis://localhost:6379/messages
-###< symfony/messenger ###
-
-###> symfony/mailer ###
-# MAILER_DSN=null://null
-###< symfony/mailer ###
-EOL
 
 echo ".env file created successfully!"
