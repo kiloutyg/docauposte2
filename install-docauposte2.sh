15d2c656b221561e20e77e2ab5b7298f63c7f606
SFDKHISSI <floriandkhissi@gmail.com>
2023-07-04 15:53:21 +0200

    tagging release candidate

--- install-docauposte2.sh
+++ install-docauposte2.sh
@@ -10,7 +10,9 @@ fi
 if [ "${ANSWER}" == "yes" ]
 then 
     sudo yum install -y git
-    
+    read -p "Are you running the app for the first Time ?(yes/no) " GIT_ADDRESS
+    git clone ${GIT_ADDRESS}
+    cd docauposte2
     bash ./prerequisites.sh
     bash ./env_create.sh
     docker compose up --build
