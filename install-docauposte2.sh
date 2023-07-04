70ce6c877a27618b7c9a3a68ce865e727763986f
SFDKHISSI <floriandkhissi@gmail.com>
2023-07-04 16:05:23 +0200

    new step to finish installation script

--- install-docauposte2.sh
+++ install-docauposte2.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
-read -p "Are you running the app for the first Time ?(yes/no) " ANSWER
+read -p "Are you running the app for the first Time ?(yes/no) " ANSWER;
 
 if [ "${ANSWER}" != "yes" ] && [ "${ANSWER}" != "no" ]
 then 
-    echo "Please answer yes or no"
-    read -p "Are you running the app for the first Time ?(yes/no) " ANSWER
+    echo "Please answer yes or no";
+    read -p "Are you running the app for the first Time ?(yes/no) " ANSWER;
 fi
 
 if [ "${ANSWER}" == "yes" ]
 then 
-    sudo yum install -y git
-    read -p "Are you running the app for the first Time ?(yes/no) " GIT_ADDRESS
-    git clone ${GIT_ADDRESS}
-    cd docauposte2
-    bash ./prerequisites.sh
-    bash ./env_create.sh
-    docker compose up --build
+    sudo yum install -y git;
+    sudo subscription-manager repo-override --repo=PlasticOmnium_Docker_Docker_CE_Stable --add=enabled:1;
+
+    sudo yum remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-engine \
+                podman \
+                runc;
+
+    sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y;
+
+    sudo groupadd docker;
+    sudo usermod -aG docker $USER;
+
+    newgrp docker;
+
+    sudo systemctl start docker;
+    sudo systemctl start containerd.service;
+    sudo systemctl enable docker.service;
+    sudo systemctl enable containerd.service;
+
+    read -p "Are you running the app for the first Time ?(yes/no) " GIT_ADDRESS;
+    git clone ${GIT_ADDRESS};
+    cd docauposte2;
+    bash ./prerequisites.sh;
+    bash ./env_create.sh;
+    docker compose up --build;
 else 
-    docker compose up
+    docker compose up;
 fi
