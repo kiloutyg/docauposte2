9ca07de497ca283eb04a90a03ed8f6c92a8bf293
SFDKHISSI <floriandkhissi@gmail.com>
2023-07-04 15:43:15 +0200

    collapsable accordion for admin and reorganization of the utility

--- migrations/Version20230704090242.php
+++ migrations/Version20230704090242.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230704081756 extends AbstractMigration
+final class Version20230704090242 extends AbstractMigration
 {
     public function getDescription(): string
     {
