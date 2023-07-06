acb095d73a0fd75ba7c4d7ac412dc2f9e116d294
SFDKHISSI <floriandkhissi@gmail.com>
2023-07-06 19:17:10 +0200

    test of the new way to do things

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,21 @@ export http_proxy='http://10.0.0.1:80';
 # composer require symfony/asset symfony/apache-pack symfony/error-handler symfony/templating symfony/ux-turbo symfony/profiler-pack symfony/var-dumper symfony/webpack-encore-bundle  symfony/ux-turbo;
 # yarn add bootstrap jquery @popperjs/core sass-loader  sass @fontsource/roboto-condensed @fortawesome/fontawesome-free axios core-js webpack encore webpack-cli webpack-notifier @symfony/webpack-encore favicon --dev;
 # yarn add bootstrap jquery @popperjs/core sass-loader  sass @fontsource/roboto-condensed @fortawesome/fontawesome-free axios core-js webpack encore webpack-cli webpack-notifier @symfony/webpack-encore favicon;
-# composer install --no-dev --optimize-autoloader;
+composer install --no-dev --optimize-autoloader;
 
-composer install;
-composer update ;
-yarn install;
-yarn upgrade;
+# composer install;
+# composer update ;
+yarn install --production;
+# yarn upgrade;
 composer clear-cache;
 chmod 777 . -R -v;
 # yarn encore production &
+php bin/console cache:clear --no-warmup --env=prod;
+php bin/console cache:warmup --env=prod;
 php bin/console make:migration;
 php bin/console doctrine:migrations:migrate;
 
-exec apache2-foreground  &
 # yarn watch &
 # yarn encore dev --watch
-yarn run encore production --progress
+yarn run encore production --progress;
+exec apache2-foreground;
