82f6f67f605b68de052de83e3cf542c63313ea7e
SFDKHISSI <floriandkhissi@gmail.com>
2023-09-05 16:24:52 +0200

    added mailer component and removed the messenger component but did not went farther that testing if it worked with the PO smtp server

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MailerController extends AbstractController
+{
+    #[Route('/mailer', name: 'app_mailer')]
+    public function index(): Response
+    {
+        return $this->render('mailer/index.html.twig', [
+            'controller_name' => 'MailerController',
+        ]);
+    }
+
+    #[Route('/email')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+
+        $emailRecipientsAddress = 'florian.dkhissi@plasticomnium.com';
+
+        $email = (new Email())
+            ->from('lan.docauposte@plasticomnium.com')
+            ->to($emailRecipientsAddress)
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return $this->redirectToRoute('app_base');
+    }
+}
