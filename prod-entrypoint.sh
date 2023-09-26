6e7bc2d94f5a3bd365f19df86018f3856192c9cd
SFDKHISSI <floriandkhissi@gmail.com>
2023-09-26 14:11:23 +0200

    test timezone

--- prod-entrypoint.sh
+++ prod-entrypoint.sh
@@ -13,12 +13,12 @@ php bin/console cache:clear --no-warmup --env=prod;
 # Warm up the cache
 php bin/console cache:warmup --env=prod;
 
-# Create the migrations directory
-mkdir -p migrations;
+# # Create the migrations directory
+# mkdir -p migrations;
 
-# Create the database and run the migrations
-php bin/console make:migration;
-php bin/console doctrine:migrations:migrate;
+# # Create the database and run the migrations
+# php bin/console make:migration;
+# php bin/console doctrine:migrations:migrate;
 
 # Build the assets
 yarn run encore production --progress;
