5a2c3a31cc1c0f173567b15899c9ea1588983388
SFDKHISSI <floriandkhissi@gmail.com>
2023-10-26 10:12:10 +0200

    route change

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -11,11 +11,11 @@ use App\Controller\UploadController;
 // This controller manage the logic of the front interface, it is the main controller of the application and is responsible for rendering the front interface.
 // It is also responsible for creating the super-admin account.
 
-#[Route('/', name: 'app_')]
+#[Route('/docauposte', name: 'app_')]
 class FrontController extends BaseController
 {
     // Render the base page
-    #[Route('/', name: 'base')]
+    #[Route('/docauposte', name: 'base')]
     public function base(): Response
     {
 
