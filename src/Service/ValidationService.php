ea525adf88cb5265971a40f902d6400e893de9a1
SFDKHISSI <floriandkhissi@gmail.com>
2024-03-05 14:25:55 +0100

    each time a validated document will be approved it will reset the concerned training records to false trained status

--- src/Service/ValidationService.php
+++ src/Service/ValidationService.php
@@ -19,47 +19,57 @@ use App\Entity\User;
 use App\Entity\Department;
 use App\Entity\Validation;
 use App\Entity\Approbation;
-
+use App\Entity\TrainingRecord;
 use App\Service\MailerService;
 use App\Service\OldUploadService;
+use App\Service\TrainingRecordService;
 
 
 class ValidationService extends AbstractController
 {
     protected $logger;
     protected $em;
+    protected $projectDir;
+
     protected $uploadRepository;
     protected $departmentRepository;
     protected $userRepository;
     protected $validationRepository;
     protected $approbationRepository;
+
     protected $mailerService;
     protected $oldUploadService;
-    protected $projectDir;
+    protected $TrainingRecordService;
 
 
     public function __construct(
         LoggerInterface                 $logger,
         EntityManagerInterface          $em,
+        ParameterBagInterface           $params,
+
         UploadRepository                $uploadRepository,
         DepartmentRepository            $departmentRepository,
         UserRepository                  $userRepository,
         ValidationRepository            $validationRepository,
         ApprobationRepository           $approbationRepository,
+
         MailerService                   $mailerService,
-        ParameterBagInterface           $params,
-        OldUploadService                $oldUploadService
+        OldUploadService                $oldUploadService,
+        TrainingRecordService           $TrainingRecordService
     ) {
         $this->logger                = $logger;
         $this->em                    = $em;
         $this->projectDir            = $params->get('kernel.project_dir');
+
         $this->uploadRepository      = $uploadRepository;
         $this->departmentRepository  = $departmentRepository;
         $this->userRepository        = $userRepository;
         $this->validationRepository  = $validationRepository;
         $this->approbationRepository = $approbationRepository;
+
         $this->mailerService         = $mailerService;
         $this->oldUploadService      = $oldUploadService;
+        $this->TrainingRecordService = $TrainingRecordService;
     }
 
     public function createValidation(Upload $upload, Request $request)
@@ -332,8 +342,8 @@ class ValidationService extends AbstractController
         }
         if ($validation->isStatus() === true) {
             $this->mailerService->sendApprovalEmail($validation);
+            $this->TrainingRecordService->updateTrainingRecord($upload);
         }
-        // Return early
         return;
     }
 
