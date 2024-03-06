5fb5d413a28a9032b581ebc6575c04588f170537
SFDKHISSI <floriandkhissi@gmail.com>
2024-03-06 15:03:55 +0100

    finished the hotwired controller to manage the message for the regex of the code operator

--- assets/controllers/operator_training_controller.js
+++ assets/controllers/operator_training_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ["newOperator", "newOperatorMessage"];
+    static targets = ["newOperator", "newOperatorMessage", "newOperatorCode", "newOperatorMessageCode"];
 
     validateNewOperator() {
         const regex = /^[a-zA-Z]+\.[a-zA-Z]+$/;
@@ -14,4 +14,19 @@ export default class extends Controller {
             this.newOperatorMessageTarget.style.color = "red"; // Display the message in red color.
         }
     }
+
+
+    validateNewOperatorCode() {
+        const regex = /^[0-9]+$/;
+        const isValid = regex.test(this.newOperatorCodeTarget.value);
+
+        if (isValid) {
+            this.newOperatorMessageCodeTarget.textContent = "";
+        } else {
+            this.newOperatorMessageCodeTarget.textContent = "Veuillez saisir un code correcte.";
+            this.newOperatorMessageCodeTarget.style.color = "red"; // Display the message in red color.
+        }
+    }
 }
+
+
