14f8c4b816881482bbe27dad88405d47cdd1d357
SFDKHISSI <floriandkhissi@gmail.com>
2024-06-26 14:56:46 +0200

    barcode and pdf generator done in base logic and correct format for the production, lack multiple ope detail by page and implementation in the application

--- src/Service/PdfGeneratorService.php
+++ src/Service/PdfGeneratorService.php
@@ -4,13 +4,8 @@ namespace App\Service;
 
 use TCPDF;
 use Picqer\Barcode\BarcodeGeneratorPNG;
-// use Picqer\Barcode\BarcodeGenerator;
-
-use Endroid\QrCode\QrCode;
-use Endroid\QrCode\Writer\PngWriter;
 
 use TomasVotruba\BarcodeBundle\Base2DBarcode;
-// use TomasVotruba\BarcodeBundle\Generator\BarcodeGenerator;
 
 class PdfGeneratorService
 {
@@ -24,7 +19,7 @@ class PdfGeneratorService
 
         // Set document information
         $pdf->SetCreator(PDF_CREATOR);
-        $pdf->SetAuthor('Your Company');
+        $pdf->SetAuthor('OPmobility Exterior Langres');
         $pdf->SetTitle('Operator Details');
         $pdf->SetSubject('Operator Information');
 
@@ -36,13 +31,10 @@ class PdfGeneratorService
         $pdf->AddPage();
 
         // Set font
-        $pdf->SetFont('helvetica', 'B', 16);
+        $pdf->SetFont('helvetica', 'B', 8);
 
-        // Add operator details
-        $pdf->Cell(0, 10, 'Full Name: ' . $operator->getName(), 0, 1);
-        $pdf->Cell(0, 10, 'Code: ' . $operator->getCode(), 0, 1);
-        $pdf->Cell(0, 10, 'UAP: ' . ($operator->getUap() ? $operator->getUap()->getName() : 'N/A'), 0, 1);
-        $pdf->Cell(0, 10, 'Team: ' . ($operator->getTeam() ? $operator->getTeam()->getName() : 'N/A'), 0, 1);
+        // Add operator Name
+        $pdf->Cell(0, 1, '' . ($operator->getName() ? $operator->getName() : 'N/A'), 0, 1, 'C', false, '', 0, false, 'T', 'T');
 
         // Generate Code128 barcode
         $generator = new BarcodeGeneratorPNG();
@@ -51,27 +43,20 @@ class PdfGeneratorService
         file_put_contents($barcodePath, $barcode);
 
         // Add barcode to PDF
-        $pdf->Image($barcodePath, 10, 50, 100, 20);
-
-        // Generate Data Matrix barcode using endroid/qr-code
-        // $qrCode = new QrCode($operator->getCode());
-        // $writer = new PngWriter();
-        // $dataMatrix = $writer->write($qrCode);
-        // $dataMatrixPath = sys_get_temp_dir() . '/datamatrix.png';
-        // file_put_contents($dataMatrixPath, $dataMatrix->getString());
+        $pdf->Image($barcodePath, 90, 14, 20, 8, '', '', 'M', false, 300, '', false, false, 0, false, false, false);
 
         // Generate Data Matrix barcode
         $dataMatrix = new Base2DBarcode();
-
         $dataMatrix->savePath = sys_get_temp_dir() . '/';
-
-
         $dataMatrixPath = $dataMatrix->getBarcodePNGPath($operator->getCode(), 'DATAMATRIX', 10, 10);
 
         // Add Data Matrix barcode to PDF
-        $pdf->Image($dataMatrixPath, 10, 80, 100, 100);
+        $pdf->Image($dataMatrixPath, 112, 14, 8, 8, '', '', 'N', false, 300, '', false, false, 0, false, false, false);
+
+        // Add operator code
+        $pdf->Cell(0, 1, '' . ($operator->getCode() ? $operator->getCode() : 'N/A'), 0, 1, 'C', false, '', 0, false, 'T', 'T');
 
         // Output the PDF
-        return $pdf->Output('S');
+        return $pdf->Output('OpeInfo_' . $operator->getName() . '.pdf', 'I');
     }
 }
