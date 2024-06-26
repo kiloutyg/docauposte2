943ba4be58670f2a834bd757225af16083984214
SFDKHISSI <floriandkhissi@gmail.com>
2024-06-26 17:00:37 +0200

    added button to prind ope detail

--- assets/js/toast.js
+++ assets/js/toast.js
@@ -6,43 +6,14 @@ document.addEventListener("turbo:load", () => {
   }
 });
 
-document.addEventListener("turbo:load", showToast);
-document.addEventListener("turbo:frame-load", showToast);
 
 
-function showToast() {
-  var toastContainer = document.getElementById("liveToast");
-  if (toastContainer) {
-    // Ensure the bootstrap object is available
-    if (typeof bootstrap !== 'undefined' && bootstrap.Toast) {
-      var toast = new bootstrap.Toast(toastContainer);
-      toast.show();
-    } else {
-      console.error('Bootstrap is not loaded');
-    }
-  }
-}
-
-// document.addEventListener("turbo:frame-load", function (event) {
-//   // Fetch and update the flash messages from the server
-//   fetch('/docauposte/flash-messages')
-//     .then(response => response.text())
-//     .then(html => {
-//       document.getElementById('flash-messages').innerHTML = html;
-//     })
-//     .catch(error => console.error('Error updating flash messages:', error));
-// });
-// document.addEventListener("turbo:load", function () {
-//   // Fetch and update the flash messages from the server
-//   fetch('/docauposte/flash-messages')
-//     .then(response => response.text())
-//     .then(html => {
-//       const flashMessagesContainer = document.getElementById('flash-messages');
-//       if (flashMessagesContainer) {
-//         flashMessagesContainer.innerHTML = html;
-//       } else {
-//         console.error('Flash messages container not found.');
-//       }
-//     })
-//     .catch(error => console.error('Error updating flash messages:', error));
-// });
+document.addEventListener('DOMContentLoaded', (event) => {
+  setTimeout(() => {
+    document.querySelectorAll('.toast').forEach((message) => {
+      message.style.transition = 'opacity 0.5s';
+      message.style.opacity = '0';
+      setTimeout(() => message.remove(), 300); // Remove from DOM after fade out
+    });
+  }, 3000); // 5 seconds
+});
