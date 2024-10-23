72316a005a8866c6bd92bcdc7b2ea532a4e05d9c
SDKHISSI <admin.sdkhissi@SX03LANLP0001.ad.ponet>
2024-10-23 11:25:36 +0200

    The situation is good enough

--- assets/controllers/operator_admin_edit_controller.js
+++ assets/controllers/operator_admin_edit_controller.js
@@ -1,4 +1,3 @@
-import { Controller } from '@hotwired/stimulus';
 import OperatorAdminCreationController from './operator_admin_creation_controller';
 import axios from 'axios';
 
@@ -15,96 +14,136 @@ export default class operatorAdminEdit extends OperatorAdminCreationController {
     ];
 
 
-
-    modifiedFieldChecker() {
-        console.log('modified field checker');
-        switch (this.operatorFormCodeTarget.value.changed) {
-            case true:
-                this.editedCodeChecker();
-                break;
-        }
+    connect() {
+        // Store initial values if needed
+        this.initialValues = {
+            initialFirstname: this.operatorFormFirstnameTarget.value,
+            initialLastname: this.operatorFormLastnameTarget.value,
+            initialCode: this.operatorFormCodeTarget.value,
+            initialTeam: this.operatorFormTeamTarget.value,
+            initialUap: this.operatorFormUapTarget.value,
+            initialIsTrainer: this.operatorFormIsTrainerTarget.checked,
+        };
     }
 
 
-    editedCodeChecker() {
 
-        console.log('editing code checker', this.operatorFormCodeTarget.value);
-
+    async editedCodeChecker() {
+        // Clear any existing timeouts to debounce user input
         clearTimeout(this.codeTypingTimeout);
-        this.codeTypingTimeout = setTimeout(() => {
 
+        this.codeTypingTimeout = setTimeout(async () => {
             const code = this.operatorFormCodeTarget.value.trim();
-            console.log('code', code);
-            const regex = /^[0-9]{5}$/;
-            let isValid = regex.test(code);
-            console.log('isValid', isValid);
+            const isValidFormat = this.isCodeValidFormat(code);
 
-            if (isValid) {
-                const response = this.editedCodeFormatChecker(code);
+            if (isValidFormat) {
+                const isValidSum = this.editedCodeFormatChecker(code);
+                console.log('isValidSum', isValidSum)
+                const isExistingCode = await this.existingCodeChecker(code);
+                console.log('isExistingCode', isExistingCode)
 
-            } else {
-                console.log('code format is not regex compliant');
-                this.operatorFormCodeTarget.value = "";
-                this.operatorFormCodeTarget.placeholder = 'Format invalide';
-            }
-        }, 800);
-    }
+                if (isValidSum && !isExistingCode) {
+                    // Code is valid and doesn't exist
+                    console.log('Code is valid and unique.');
+                    // Provide positive feedback to the user if necessary
+                } else if (!isValidSum) {
+                    console.log('Sum of first three digits does not equal last two digits.');
+                    this.showCodeFormatError('Bad Format.');
 
+                        const newCode = await this.proposeCompliantNewCode();
+                        this.operatorFormCodeTarget.value = newCode;
+                } else if (isExistingCode) {
+                    console.log('Code already exists.');
+                    this.showCodeExistenceError('Existe déjà.');
 
-    existingCodeChecker(code) {
-        console.log('checking generated code');
-        return axios.post(`/docauposte/operator/check-if-code-exist`, { code: code });
-    }
-
-
-
-    editedCodeFormatChecker() {
-
-        clearTimeout(this.codeFormatCheckerTimeout);
-        this.codeFormatCheckerTimeout = setTimeout(() => {
-
-            console.log('editing code format checker');
-            const code = this.operatorFormCodeTarget.value.trim();
-            const sumOfFirstThreeDigit = code
-                .toString()
-                .split('')
-                .slice(0, 3)
-                .reduce((sum, digit) => sum + Number(digit), 0);
-            console.log('sum of first three digits:', sumOfFirstThreeDigit);
-            const valueOfLastTwoDigit = code.toString()
-                .split('')
-                .slice(3)
-                .join('');
-            console.log('value of last two digits:', valueOfLastTwoDigit);
-
-            if (sumOfFirstThreeDigit === valueOfLastTwoDigit) {
-                try {
-                    const reponse = this.existingCodeChecker(code);
-                    if (reponse.data.found) {
-                        console.log('code already exists');
-                        this.operatorFormCodeTarget.value = "";
-                        this.operatorFormCodeTarget.placeholder = 'Code déjà utilisé';
-                    } else {
-                        console.log('code does not exist and is valid ');
-                        return;
-                    }
-                } catch (error) {
-                    console.error('Error checking for duplicate operator code.', error);
+                        const newCode = await this.proposeCompliantNewCode();
+                        this.operatorFormCodeTarget.value = newCode;
                 }
             } else {
-                console.log('sum of first three digits is not equal to value of last two digits');
-                clearTimeout(this.codeFormatCheckerNewCodeMessageTimeout);
-                this.codeFormatCheckerNewCodeMessageTimeout = setTimeout(() => {
-                    this.operatorFormCodeTarget.value = "";
-                    this.operatorFormCodeTarget.placeholder = 'Code Auto 8sec';
-                    clearTimeout(this.codeFormatCheckerNewCodeTimeout);
-                    this.codeFormatCheckerNewCodeTimeout = setTimeout(() => {
-                        const newCode = this.codeGenerator();
-                        console.log('newCode', newCode);
-                        this.operatorFormCodeTarget.value = newCode;
-                    }, 800);
-                }, 800);
+                console.log('Code format is not compliant.');
+                this.showCodeFormatError('Format invalide.');
+
+                    const newCode = await this.proposeCompliantNewCode();
+                    this.operatorFormCodeTarget.value = newCode;
             }
         }, 800);
     }
+
+
+
+    // Helper method to check if the code format is valid
+    isCodeValidFormat(code) {
+        const regex = /^[0-9]{5}$/;
+        return regex.test(code);
+    }
+
+
+
+    // Method to display format errors
+    showCodeFormatError(message) {
+            this.operatorFormCodeTarget.value = '';
+            this.operatorFormCodeTarget.placeholder = message;
+    }
+
+
+
+    // Method to display existence errors
+    showCodeExistenceError(message) {
+            // You can implement visual feedback to the user here
+            this.operatorFormCodeTarget.value = '';
+            this.operatorFormCodeTarget.placeholder = message;
+    }
+
+
+
+    async existingCodeChecker(code) {
+        // Since axios.post is asynchronous, we need to handle it with async/await or promises
+        console.log('checking if code already exists, code:', code);
+        return axios.post('/docauposte/operator/check-if-code-exist', { code })
+            .then(response => {
+                console.log('response', response)
+                console.log('response.data.found', response.data.found)
+                const found = response.data.found;
+                console.log('found', found)
+                return found;
+            })
+            .catch(error => {
+                console.error('Error checking for duplicate operator code.', error);
+                // Handle error appropriately
+                return false;
+            });
+    }
+
+
+
+    editedCodeFormatChecker(code) {
+        console.log('editing code format checker, code:', code);
+        const sumOfFirstThreeDigits = code
+            .substring(0, 3)
+            .split('')
+            .reduce((sum, digit) => sum + Number(digit), 0);
+
+        const lastTwoDigitsValue = Number(code.substring(3));
+
+        return sumOfFirstThreeDigits === lastTwoDigitsValue;
+    }
+
+
+
+    async proposeCompliantNewCode() {
+        console.log('proposing a compliant new code');
+
+        const newCode = this.codeGenerator();
+        console.log('newCode', newCode);
+        const response = await this.existingCodeChecker(newCode);
+        if (response) {
+            console.log('newCode already exists, proposing a new one');
+            return this.proposeCompliantNewCode();
+        } else {
+            return newCode;
+        }
+
+    }
 }
+
+
