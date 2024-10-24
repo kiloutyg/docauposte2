bffa4854b22c8f3a07733468b8aa92f096e349d9
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2024-10-24 17:17:30 +0200

    typo in proxy_env variabl in env_update_popdman script and added an echo to the end of the update process

--- install-docauposte2.sh
+++ install-docauposte2.sh
@@ -159,6 +159,13 @@ done
             else
                 bash ./env_update_podman.sh ${GITHUB_USER};
                 podman play kube --replace ./dap.yml;
+                # Wait until the webpack compiled successfully
+                until podman logs --since 10s --tail 10 web-docauposte 2>&1 | grep -q "webpack compiled successfully"; do
+                echo "Waiting for the webpack to be compiled" 
+                sleep 10
+                done
+                echo "Docauposte2 updated successfully";
+
             fi
         fi
     fi
