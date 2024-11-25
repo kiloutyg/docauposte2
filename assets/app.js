39129b40a37d369f72a5f711658cdd5e4a316a4d
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2024-11-25 08:54:35 +0100

    rollback to previous dependency tree version

--- assets/app.js
+++ assets/app.js
@@ -27,25 +27,8 @@ document.addEventListener('turbo:submit-start', (event) => {
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.scss";
 
-// start the Stimulus application
 import "./bootstrap";
 
-// Import jQuery and Popper.js
-// import $ from "jquery";
-// import { createPopper } from "@popperjs/core";
-
-// // Make jQuery and Popper.js available globally
-// global.$ = global.jQuery = $;
-// global.createPopper = createPopper;
-
-// Import Bootstrap's JavaScript
 import "bootstrap";
 
-// // Import the Select2 library
-// import "select2";
-
-// // initialize Select2 on your elements
-// $(document).ready(function () {
-//     $('.select2-enable').select2();
-// });
 
