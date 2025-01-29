b3c67e8b5109f212beca170fa2d30f7c7cab4e30
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-01-29 10:12:11 +0100

    little modification to remove issues and keep a low code smell

--- env_create_podman.sh
+++ env_create_podman.sh
@@ -180,9 +180,6 @@ MESSENGER_TRANSPORT_DSN=doctrine://default?auto_setup=0
 ###> doctrine/doctrine-bundle ###
 # Format described at https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connecting-using-a-url
 # IMPORTANT: You MUST configure your server version, either here or in config/packages/doctrine.yaml
-#
-# DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db"
-# DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=8&charset=utf8mb4"
 
 DATABASE_URL=mysql://root:\${MYSQL_ROOT_PASSWORD}@database/\${MYSQL_DATABASE}?charset=utf8mb4&serverVersion=MariaDB-11.6.2&sslmode=verify_ca&sslrootcert=/etc/ssl/certs/ca-cert.pem
 
