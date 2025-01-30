dd0a3157c4994e47169c37f93e570797c91d1162
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-01-30 11:08:53 +0100

    testing if .env.test is the issue

--- fix-permissions-dap.sh
+++ fix-permissions-dap.sh
@@ -3,8 +3,10 @@
 # Set permissions and ownership
 chmod 750 /var/www/public/doc
 chown -R www-data:www-data /var/www/public/doc
-chmod 640 /var/www/.env
-chown www-data:www-data /var/www/.env
+# chmod 640 /var/www/.env
+# chown www-data:www-data /var/www/.env
+chmod 640 /dap-certs/.env
+chown www-data:www-data /dap-certs/.env
 chmod 644 /dap-certs/ca-cert.pem /dap-certs/server-cert.pem
 chmod 644 /dap-certs/server-key.pem
 chown www-data:www-data /dap-certs/*.pem
