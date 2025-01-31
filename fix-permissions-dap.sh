d96bbae264f46ac0c7b5726d8d4a1ad6c781e7b5
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-01-31 15:52:26 +0100

    Definitive version of the fix-permissions-dap script

--- fix-permissions-dap.sh
+++ fix-permissions-dap.sh
@@ -3,8 +3,6 @@
 # Set permissions and ownership
 chmod 750 /var/www/public/doc
 chown -R www-data:www-data /var/www/public/doc
-# chmod 640 /var/www/.env
-# chown www-data:www-data /var/www/.env
 chmod 640 /dap-certs/.env
 chown www-data:www-data /dap-certs/.env
 chmod 644 /dap-certs/ca-cert.pem /dap-certs/server-cert.pem
