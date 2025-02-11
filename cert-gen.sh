1187422ebb7ccf4022908ebe12b7618a72de801b
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-02-11 11:11:24 +0100

    Changed the cert generator script to be more good practice firendly, added the option to not check CA validity since it seems to be what need to be done to allow connection to the db

--- cert-gen.sh
+++ cert-gen.sh
@@ -13,11 +13,11 @@ openssl req -new -x509 -nodes -days 3600 \
 
 
 
-# Generate server certificate
+# Generate server certificate with an appropriate subject and unique serial number
 openssl req -newkey rsa:2048 -days 3600 \
     -nodes -keyout ./secrets/ssl/server-key.pem \
     -out ./secrets/ssl/server-req.pem \
-    -subj "/CN=database"
+    -subj "/CN=docauposte-database-pod"
 
 openssl x509 -req -in ./secrets/ssl/server-req.pem -days 3600 \
     -CA ./secrets/ssl/ca-cert.pem -CAkey ./secrets/ssl/ca-key.pem -set_serial 01 \
@@ -25,12 +25,12 @@ openssl x509 -req -in ./secrets/ssl/server-req.pem -days 3600 \
 
 
 
-# Generate client certificate
+# Generate client certificate with a different serial number
 openssl req -newkey rsa:2048 -days 3600 \
     -nodes -keyout ./secrets/ssl/client-key.pem \
     -out ./secrets/ssl/client-req.pem \
     -subj "/CN=MariaDB Client"
 
 openssl x509 -req -in ./secrets/ssl/client-req.pem -days 3600 \
-    -CA ./secrets/ssl/ca-cert.pem -CAkey ./secrets/ssl/ca-key.pem -set_serial 01 \
+    -CA ./secrets/ssl/ca-cert.pem -CAkey ./secrets/ssl/ca-key.pem -set_serial 02 \
     -out ./secrets/ssl/client-cert.pem
