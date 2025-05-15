30e2c9da9369662aac5364d89a67106790978b69
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-05-15 11:57:40 +0200

    commit pre merge

--- migrations/Version20250515092251.php
+++ migrations/Version20250515092251.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250414121704 extends AbstractMigration
+final class Version20250515092251 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,20 @@ final class Version20250414121704 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD operator_id INT DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_CD1DE18A5E237E06 ON department (name)');
+        $this->addSql('ALTER TABLE user ADD operator_id INT DEFAULT NULL, CHANGE email_address email_address VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649584598A3 FOREIGN KEY (operator_id) REFERENCES operator (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649B08E074E ON user (email_address)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649584598A3 ON user (operator_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_CD1DE18A5E237E06 ON department');
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649584598A3');
+        $this->addSql('DROP INDEX UNIQ_8D93D649B08E074E ON user');
         $this->addSql('DROP INDEX UNIQ_8D93D649584598A3 ON user');
-        $this->addSql('ALTER TABLE user DROP operator_id');
+        $this->addSql('ALTER TABLE user DROP operator_id, CHANGE email_address email_address VARCHAR(255) DEFAULT NULL');
     }
 }
