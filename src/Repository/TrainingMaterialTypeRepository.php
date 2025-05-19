7b3798e35142fbffd09f2881a8ca1912de3b2f3c
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-05-19 17:44:39 +0200

    Changed where the li tags for the shift leaders are located, deleted commented code in training material type repository

--- src/Repository/TrainingMaterialTypeRepository.php
+++ src/Repository/TrainingMaterialTypeRepository.php
@@ -16,28 +16,4 @@ class TrainingMaterialTypeRepository extends ServiceEntityRepository
         parent::__construct($registry, TrainingMaterialType::class);
     }
 
-    //    /**
-    //     * @return TrainingMaterialType[] Returns an array of TrainingMaterialType objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('t.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?TrainingMaterialType
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
