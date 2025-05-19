8b2988d138e466c6a469f98594a2c44518de4a0f
Sofian Florian DKHISSI <30991548+kiloutyg@users.noreply.github.com>
2025-05-19 10:40:32 +0200

    Update entrypoint.sh

--- docker/dockerfileGHA.prod/entrypoint.sh
+++ docker/dockerfileGHA.prod/entrypoint.sh
@@ -16,7 +16,9 @@ yarn install --ignore-scripts --production
 composer clear-cache
 
 # Run the migrations
-# php bin/console doctrine:migrations:migrate --no-all-or-nothing;
+php bin/console doctrine:migrations:diff --no-interaction;
+php bin/console doctrine:migrations:migrate --no-interaction --no-all-or-nothing;
+
 
 # Clear and warm up Symfony cache
 php ./bin/console cache:clear --no-warmup --env=prod
